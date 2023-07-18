7495b5278c13f9f3af970aed98433b723bfd7338
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-18 23:25:43 +0200

    Create AppHost.cs

---
+++ WpfOidcClient/WpfOidcClient/AppHost.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using TIKSN.DependencyInjection;
+
+namespace WpfOidcClient
+{
+    public static class AppHost
+    {
+        private static Lazy<IServiceProvider> serviceProvider
+            = new Lazy<IServiceProvider>(CreateServiceProvider, LazyThreadSafetyMode.ExecutionAndPublication);
+
+        public static T GetRequiredService<T>() where T : notnull
+        {
+            return serviceProvider.Value.GetRequiredService<T>();
+        }
+
+        private static IServiceProvider CreateServiceProvider()
+        {
+            var builder = Host.CreateApplicationBuilder();
+
+            RegisterServices(builder.Services);
+
+            var host = builder.Build();
+
+            return host.Services;
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            services.AddFrameworkPlatform();
+        }
+    }
+}
