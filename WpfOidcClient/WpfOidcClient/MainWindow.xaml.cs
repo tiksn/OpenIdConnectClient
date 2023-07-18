c5e31409be76a59d2643566165bed8523b727fad
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-18 10:22:36 +0200

    Ported WpfWebView2 Sample

---
+++ WpfOidcClient/WpfOidcClient/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using IdentityModel.OidcClient;
+using System;
+using System.Windows;
+
+namespace WpfOidcClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var options = new OidcClientOptions()
+            {
+                Authority = "https://demo.duendesoftware.com/",
+                ClientId = "interactive.public",
+                Scope = "openid profile email",
+                RedirectUri = "http://127.0.0.1/sample-wpf-app",
+                Browser = new WpfEmbeddedBrowser(),
+                Policy = new Policy
+                {
+                    RequireIdentityTokenSignature = false
+                }
+            };
+
+            var _oidcClient = new OidcClient(options);
+
+            LoginResult loginResult;
+            try
+            {
+                loginResult = await _oidcClient.LoginAsync();
+            }
+            catch (Exception exception)
+            {
+                txbMessage.Text = $"Unexpected Error: {exception.Message}";
+                return;
+            }
+
+            if (loginResult.IsError)
+            {
+                txbMessage.Text = loginResult.Error == "UserCancel" ? "The sign-in window was closed before authorization was completed." : loginResult.Error;
+            }
+            else
+            {
+                txbMessage.Text = loginResult.User.Identity.Name;
+            }
+        }
+    }
+}
