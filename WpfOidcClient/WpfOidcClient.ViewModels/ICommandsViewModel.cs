e6ca10b8aad60e62e237f574fc50995214ff802c
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-19 23:56:21 +0200

    Update ICommandsViewModel.cs

--- WpfOidcClient/WpfOidcClient.ViewModels/ICommandsViewModel.cs
+++ WpfOidcClient/WpfOidcClient.ViewModels/ICommandsViewModel.cs
@@ -1,5 +1,12 @@
-﻿namespace WpfOidcClient.ViewModels;
+﻿using IdentityModel.OidcClient;
+using ReactiveUI;
+using System.Reactive;
+
+namespace WpfOidcClient.ViewModels;
 
 public interface ICommandsViewModel
 {
+    ReactiveCommand<Unit, LoginResult> LogInCommand { get; }
+
+    ReactiveCommand<Unit, Unit> LogOutCommand { get; }
 }
