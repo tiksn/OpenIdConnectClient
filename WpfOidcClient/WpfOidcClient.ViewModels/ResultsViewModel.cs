4703acb88e7b46bb7ec9a87d17deebd3b188096a
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-20 15:26:00 +0200

    Update ResultsViewModel.cs

--- WpfOidcClient/WpfOidcClient.ViewModels/ResultsViewModel.cs
+++ WpfOidcClient/WpfOidcClient.ViewModels/ResultsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using ReactiveUI;
+﻿using IdentityModel.OidcClient;
+using ReactiveUI;
+using System.Reactive.Linq;
 
 namespace WpfOidcClient.ViewModels;
 
@@ -6,5 +8,18 @@ public class ResultsViewModel : ViewModel, IResultsViewModel
 {
     public ResultsViewModel(IMessageBus messageBus) : base(messageBus)
     {
+        _accessToken = messageBus
+            .Listen<LoginResult>()
+            .Select(x => x.AccessToken)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .ToProperty(this, x => x.AccessToken);
     }
+
+    #region Access Token property
+
+    private readonly ObservableAsPropertyHelper<string> _accessToken;
+
+    public string AccessToken => _accessToken.Value;
+
+    #endregion Access Token property
 }
