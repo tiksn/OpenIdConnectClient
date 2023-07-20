15c6d9c2389d9d7d8f07bc405cb122b44da3bb4a
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-20 15:56:52 +0200

    Update IResultsViewModel.cs

--- WpfOidcClient/WpfOidcClient.ViewModels/IResultsViewModel.cs
+++ WpfOidcClient/WpfOidcClient.ViewModels/IResultsViewModel.cs
@@ -4,6 +4,8 @@ public interface IResultsViewModel
 {
     public string AccessToken { get; }
 
+    public string AccessTokenExpiration { get; }
+
     public string IdentityToken { get; }
 
     public string RefreshToken { get; }
