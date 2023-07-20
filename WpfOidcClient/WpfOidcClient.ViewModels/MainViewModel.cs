433d9e3fba73caa12adc073a34fb73309157ab1f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-20 15:06:55 +0200

    Update MainViewModel.cs

--- WpfOidcClient/WpfOidcClient.ViewModels/MainViewModel.cs
+++ WpfOidcClient/WpfOidcClient.ViewModels/MainViewModel.cs
@@ -7,13 +7,17 @@ public class MainViewModel : ViewModel, IMainViewModel
     public MainViewModel(
         IClientOptionsViewModel clientOptions,
         ICommandsViewModel commands,
+        IResultsViewModel results,
         IMessageBus messageBus) : base(messageBus)
     {
         ClientOptions = clientOptions ?? throw new ArgumentNullException(nameof(clientOptions));
         Commands = commands ?? throw new ArgumentNullException(nameof(commands));
+        Results = results ?? throw new ArgumentNullException(nameof(results));
     }
 
     public IClientOptionsViewModel ClientOptions { get; }
 
     public ICommandsViewModel Commands { get; }
+
+    public IResultsViewModel Results { get; }
 }
