b84f35ab6cc8a3209cc903fb6e93c13a83be292c
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-10-21 01:45:02 +0200

    Update AppBootstrapper.cs

--- OpenIdConnectClient.Maui/AppBootstrapper.cs
+++ OpenIdConnectClient.Maui/AppBootstrapper.cs
@@ -1,12 +1,4 @@
-﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for full license information.
-
-using OpenIdConnectClient.Maui.Views;
-using OpenIdConnectClient.ViewModels;
-using ReactiveUI;
-using Splat;
+﻿using ReactiveUI;
 
 namespace OpenIdConnectClient.Maui
 {
@@ -22,9 +14,7 @@ namespace OpenIdConnectClient.Maui
         {
             var router = new RoutingState();
             var screen = new AppBootstrapScreen(router);
-            Locator.CurrentMutable.RegisterConstant(screen, typeof(IScreen));
             builder.Services.AddSingleton<IScreen>(screen);
-            Locator.CurrentMutable.Register(() => new AboutView(), typeof(IViewFor<AboutViewModel>));
 
             return builder;
         }
